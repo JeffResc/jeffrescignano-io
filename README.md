673ade8780f24e70059b4a724b772145fa0f8516
Jeff Rescignano <jeff@jeffresc.dev>
2025-09-18 01:10:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JeffRescignano.io
+My personal portfolio website.
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for more details.
---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+import cloudflare from '@astrojs/cloudflare';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://jeffrescignano.io',
+  base: '/',
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare(),
+  output: 'static',
+});
